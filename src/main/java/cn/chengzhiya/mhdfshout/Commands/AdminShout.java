2aabca8878d78c8fe0e3b26d55d3846c58e3cf95
ChengZhiYa <292200693@qq.com>
2024-03-24 13:07:48 +0800

    提交插件

--- src/main/java/cn/chengzhiya/mhdfshout/Commands/AdminShout.java
+++ src/main/java/cn/chengzhiya/mhdfshout/Commands/AdminShout.java
@@ -14,12 +14,14 @@ import static cn.chengzhiya.mhdfshout.Util.*;
 public final class AdminShout implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            player.sendMessage(i18n("InputMessage").replaceAll("\\{Exit\\}", Objects.requireNonNull(main.main.getConfig().getString("InputSettings.ExitMessage"))));
-            getShoutHashMap().put(player.getName(), "AdminShout");
-        } else {
-            sender.sendMessage(i18n("OnlyPlayer"));
+        if (sender.hasPermission("MHDFShout.AdminShout")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage(i18n("InputMessage").replaceAll("\\{Exit\\}", Objects.requireNonNull(main.main.getConfig().getString("InputSettings.ExitMessage"))));
+                getShoutHashMap().put(player.getName(), "AdminShout");
+            } else {
+                sender.sendMessage(i18n("OnlyPlayer"));
+            }
         }
         return false;
     }
